4ef6f756a45158a09a927f1298ae1887f3c8c9b2
Dalosa <ramananasy@yahoo.com>
2022-12-09 08:58:04 +0100

    Initial commit

---
+++ app/src/main/java/com/dalosa/myapl2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.dalosa.myapl2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dalosa.myapl2.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/dalosa/myapl2/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.dalosa.myapl2.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
